d9424540ad0544dcf78a00f6c5dc99986cb693b3
MarkSmirnov13 <marksmirnov13@gmail.com>
2020-05-16 22:19:43 +0000

    Deploying to gh-pages from  @ ff5730ff6087723a74b718107728ca3a1fcb4182 🚀

---
+++ precache-manifest.6eb6192c59308e186cb46cc16b518026.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c700a915f61e20e6f5ae21ad310ed8ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a45db61ff297a92297",
+    "url": "/static/css/main.38fa1f2a.chunk.css"
+  },
+  {
+    "revision": "3e254559f2481f33aba0",
+    "url": "/static/js/2.d04ee391.chunk.js"
+  },
+  {
+    "revision": "d39744b49cd97a6a5d7904677be33b38",
+    "url": "/static/js/2.d04ee391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a45db61ff297a92297",
+    "url": "/static/js/main.3e33c31f.chunk.js"
+  },
+  {
+    "revision": "f62bb0204421bc128baa",
+    "url": "/static/js/runtime-main.0f1a69a8.js"
+  },
+  {
+    "revision": "ee96ab1b14784d5d3eca82b0d23b4313",
+    "url": "/static/media/blue_mask.ee96ab1b.png"
+  },
+  {
+    "revision": "cff87dbe3a11e01a2ad9f03b9022abef",
+    "url": "/static/media/elon_musk.cff87dbe.png"
+  }
+]);
