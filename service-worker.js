06c7c62650c06bc874be2736248a899eeb7e889d
MarkSmirnov13 <marksmirnov13@gmail.com>
2020-05-17 15:01:05 +0000

    Deploying to gh-pages from  @ 1213ebd7212eeb0ae2a7883bcf44d8326e9b5208 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Muskify/precache-manifest.46bc2cedc40cbcad88375ddd0a7de7fe.js"
+  "/Muskify/precache-manifest.69bfc4cdbc475b3361c28e243cff6d88.js"
 );
 
 self.addEventListener('message', (event) => {
