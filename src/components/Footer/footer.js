e931bb96f5c888399882840c4e9fbf77bef75df0
mark-smirnov <mark-smirnov@yandex-team.ru>
2020-05-17 17:18:13 +0300

    fix: added Emoji component

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Emoji from '../Emoji';
+
 import './footer.css';
 
 const Footer = () => (
     <footer>
         <p>
-            Created with 🤪 by <a href='https://vk.com/marksmirnov13'>Mark Smirnov</a>
+            Created with <Emoji symbol="🤪" label="zany-face"/> by <a href='https://vk.com/marksmirnov13'>Mark Smirnov</a>
         </p>
     </footer>
 );
