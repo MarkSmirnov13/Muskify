db3e77154a4883b2c74830066eda96568b42fe66
mark-smirnov <mark-smirnov@yandex-team.ru>
2020-05-23 12:21:07 +0300

    feat: изменен футер

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -7,7 +7,8 @@ import './footer.css';
 const Footer = () => (
     <footer>
         <p>
-            Created with <Emoji symbol="🤪" label="zany-face"/> by <a href='https://vk.com/marksmirnov13'>Mark Smirnov</a>
+            Created with <Emoji symbol="🤪" label="zany-face"/> by <a href='https://vk.com/marksmirnov13'>Mark Smirnov</a>,
+            <a href='https://vk.com/o.butenko98'>Butenko Oleg</a> and <a href='https://vk.com/lukianchukma'>Lukianchuk Maxim</a>
         </p>
     </footer>
 );
